9bb4e1fad34e38a446db6174927a2c58ffa98728
Coulibaly Nonta <coulibalynonta@gmail.com>
2016-08-05 18:48:10 +0100

    Upload blobs for cm-13.0

---
+++ d5110-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/d5110/proprietary/vendor,system/vendor)
