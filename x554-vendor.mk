5e03eb3951b77563743e698fc425a815a457787e
Raibatsu <haider2090@gmail.com>
2017-06-15 11:22:07 +0500

    For HOT 3

---
+++ x554-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/x554/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/x554/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/x554/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/x554/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/infinix/x554/xbin,system/xbin)
+
